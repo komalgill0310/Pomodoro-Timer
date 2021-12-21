c457083a75a8441d4ed5f4b8149b9c0b57584be0
Komal Gill <komalgill0310@gmail.com>
2021-12-21 04:02:08 -0500

    Start, Stor, reset is working for Work Timer

--- script.js
+++ script.js
@@ -1,41 +1,74 @@
 let restMinute = 2;
 let workMinute = 10;
 let second = 60;
+let start = false;
+let workTimer;
+let restTimer;
 
 let restTime = document.getElementById("rest-time");
 let workTime = document.getElementById("work-time");
 
+const radioButtons = document.querySelectorAll('form');
+
+function startToSTopText() {//working fine
+  document.getElementById('start').innerHTML = 'Stop';
+  runCountDown();
+  document.getElementById('reset').disabled = true;
+  console.log('startToStop');
+  start = true;
+}
+
+function stopToStartText() {//this function is not working as expected
+  // let myTimer = runCountDown();
+  document.getElementById('start').innerHTML = 'Start';
+  clearInterval(workTimer);
+  document.getElementById('reset').disabled = false;
+  console.log('stopToStart');
+  start = false;
+}
+
+//run function based on condition
+function run(){
+  return start ? stopToStartText() : startToSTopText();
+}
+
+//do CountDown
+function runCountDown() {
+  // if(document.getElementById('work').checked){
+  workTimer = setInterval(workCountDown, 1000);
+  // }
+  // else{
+   restTimer = setInterval(restCountDown, 1000);
+// }
+  return (document.getElementById('work').checked) ? workTimer : restTimer;
+}
+
 //run timer here!
 document.getElementById("start").addEventListener('click', (e) => {
   e.preventDefault(); 
-  runCountDown();
-  // stop();
+  run();
 });
 
-//stop the timer logic here!
+document.getElementById('reset').addEventListener('click', (e) => {
+  e.preventDefault();
+  minute = 10;
+  second = 0;
+  workTime.innerHTML = `${workMinute}:0${second}`;
+});
 
-// function stop(){
-//   let stopTime = stopTimer();
-//   document.getElementById('start').textContent = "Stop";
-//   if(document.getElementById("start").click()){
-//     return stopTime;
-//   }
-// }
-
-const radioButtons = document.querySelectorAll('form');
-
-radioButtons.forEach(function(radioButton){
-  document.getElementById('break').style.display = 'none';
+//display timer text under desired section
+radioButtons.forEach(function (radioButton) {
+  // document.getElementById('break').style.display = 'none';
   radioButton.addEventListener('click', () => {
-    if(document.getElementById('work').checked){
+    if (document.getElementById('work').checked) {
       document.getElementById('work-min').style.display = 'block';
       document.getElementById('break').style.display = 'none';
-      // console.log('successful');
+      // console.log('work');
     }
-    else{
+    else {
       document.getElementById('break').style.display = 'block';
       document.getElementById('work-min').style.display = 'none';
-      // console.log('this also ran successfully');
+      // console.log('rest');
     }
   });
 });
@@ -86,13 +119,6 @@ function workCountDown() {
   }
 }
 
-function runCountDown() {
-  let workTimer = setInterval(workCountDown, 1000);
-  let restTimer = setInterval(restCountDown, 1000);
-  return (document.getElementById('work').checked) ? workTimer : restTimer;
-}
 
-// function stopTimer() {
-//   let myTimer = runCountDown();
-//  return clearInterval(myTimer);
-// }
+
+
