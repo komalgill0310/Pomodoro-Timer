1ddc3ec72ae18d153ce3e255c23cb358e00373d2
Komal Gill <komalgill0310@gmail.com>
2022-01-05 01:35:09 -0500

    pomodoro functionality has completed except Audio

---
+++ readMe.md
@@ -0,0 +1,13 @@
+# **Pomodoro Timer**
+
+<font size = 6> This application is simply written in ***JavaScript, HTML and CSS***</font>
+
+<font size=5> Play ***[here](https://komalgill0310.github.io/Rock-Paper-Scissors/)***</font>
+
+<img src = "rps app screenshot.PNG">
+
+# **Summary**
+<font size=5>Pomodoro Timer is a basic application.
+
+# Author
+<font size=5>Komal - | Software Developer | [Rock Paper Scissor](https://komalgill0310.github.io/Rock-Paper-Scissors/) | [LinkedIn](www.linkedin.com/in/komalpreet-kaur-3b6924177)
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let restSecond = 60;
 let workSecond = 60;
-let minute = 25;
+let workMinute = 25;
 let restMinute = 5;
 
 let workInterval;
@@ -10,7 +10,7 @@ let workTime = document.getElementById('work-time');
 let restTime = document.getElementById('rest-time');
 
 const radioButtons = document.querySelectorAll('form');
-let start = false;//button is not clicked
+let startButton = false;
 
 document.getElementById("start").addEventListener('click', (e) => {
   e.preventDefault();
@@ -21,7 +21,7 @@ document.getElementById("start").addEventListener('click', (e) => {
     stopInterval();
   }
   changeTextOfStartButton();
-  disableRestHtml();
+  disableHtmlOnClickedStartButton();
 });
 
 document.getElementById("reset").addEventListener('click', (e) => {
@@ -34,23 +34,9 @@ function runWorkTimer() {
   return workInterval;
 }
 
-function displayWorkTime() {
-  let workMinute = workCountDown();
-  let workSecondTime = workSecondCountDown();
-  workTime.textContent = `${workMinute}:${workSecondTime}`;
-  return workTime;
-}
-
 function runRestTimer() {
   restInterval = setInterval(displayRestTime, 1000);
   return restInterval;
-}
-
-function displayRestTime() {
-  let restMinuteCount = restMinuteCountDown();
-  let restSecondTime= restSecondCountDown();
-  restTime.textContent = `${restMinuteCount}:${restSecondTime}`;
-  return restTime;
 }
 
 function stopInterval() {
@@ -62,12 +48,35 @@ function stopInterval() {
   }
 }
 
+function changeTextOfStartButton() {
+  if (!startButton) {
+    document.getElementById('start').textContent = 'Stop';
+    startButton = true;
+  }
+  else {
+    document.getElementById('start').textContent = 'Start';
+    startButton = false;
+  }
+}
+
+function disableHtmlOnClickedStartButton() {
+  if (startButton) {
+    document.getElementById('rest').disabled = true;
+    document.getElementById('work').disabled = true;
+    document.getElementById('reset').disabled = true;
+  }
+  else {
+    document.getElementById('rest').disabled = false;
+    document.getElementById('work').disabled = false;
+    document.getElementById('reset').disabled = false;
+  }
+}
+
 function resetTimer() {
-  // second = 0;
   if (document.getElementById('work').checked) {
-    minute = 25;
+    workMinute = 25;
     workSecond = 0;
-    workTime.textContent = `${minute}:0${workSecond}`;
+    workTime.textContent = `${workMinute}:0${workSecond}`;
     return workTime;
   }
   else {
@@ -78,21 +87,35 @@ function resetTimer() {
   }
 }
 
-function restSecondCountDown() {
-  if (restSecond > 10 && restSecond <= 60) {
-    restSecond--;
-    return restSecond;
+function displayWorkTime() {
+  let workMinuteCount = workCountDown();
+  let workSecondCount = workSecondCountDown();
+  workTime.textContent = `${workMinuteCount}:${workSecondCount}`;
+  return workTime;
+}
+
+function displayRestTime() {
+  let restMinuteCount = restMinuteCountDown();
+  let restSecondCount = restSecondCountDown();
+  restTime.textContent = `${restMinuteCount}:${restSecondCount}`;
+  return restTime;
+}
+
+function workCountDown() {
+  if (workSecond === 0 && workMinute > 0) {
+    workMinute--;
+    workSecond = 60;
+    if ((minute >= 0 && workMinute < 10)) {
+      return `0${workMinute}`;
+    }
   }
-  else if ((restSecond > 0 && restSecond <= 10)) {
-    restSecond--;
-    return `0${restSecond}`;
-  }
-  else if (restSecond === 0 && minute === 0) {
-    return;
+  else if ((workMinute >= 0 && workMinute < 10)) {
+    return `0${workMinute}`;
   }
   else {
-    return `00`;
+    `${workMinute}`;
   }
+  return workMinute;
 }
 
 function workSecondCountDown() {
@@ -112,40 +135,6 @@ function workSecondCountDown() {
   }
 }
 
-// function secondCountDown() {
-//   if (second > 10 && second <= 60) {
-//     second--;
-//     return second;
-//   }
-//   else if ((second > 0 && second <= 10)) {
-//     second--;
-//     return `0${second}`;
-//   }
-//   else if (second === 0 && minute === 0) {
-//     return;
-//   }
-//   else {
-//     return `00`;
-//   }
-// }
-
-function workCountDown() {
-  if (workSecond === 0 && minute > 0) {
-    minute--;
-    workSecond = 60;
-    if ((minute >= 0 && minute < 10)) {
-      return `0${minute}`;
-    }
-  }
-  else if ((minute >= 0 && minute < 10)) {
-    return `0${minute}`;
-  }
-  else {
-    `${minute}`;
-  }
-  return minute;
-}
-
 function restMinuteCountDown() {
   if (restSecond === 0 && restMinute > 0) {
     restMinute--;
@@ -155,10 +144,26 @@ function restMinuteCountDown() {
   else {
     `${restMinute}`;
   }
-  return '0' + restMinute;
+  return `0${restMinute}`;
 }
 
-//display timer text under desired section
+function restSecondCountDown() {
+  if (restSecond > 10 && restSecond <= 60) {
+    restSecond--;
+    return restSecond;
+  }
+  else if ((restSecond > 0 && restSecond <= 10)) {
+    restSecond--;
+    return `0${restSecond}`;
+  }
+  else if (restSecond === 0 && minute === 0) {
+    return;
+  }
+  else {
+    return `00`;
+  }
+}
+
 radioButtons.forEach(function (radioButton) {
   restTime.style.display = 'none';
   radioButton.addEventListener('click', () => {
@@ -173,32 +178,7 @@ radioButtons.forEach(function (radioButton) {
   });
 });
 
-//disable rest of the elements when Clicked Start Button
-function disableRestHtml() {
-  if (start) {
-    document.getElementById('rest').disabled = true;
-    document.getElementById('work').disabled = true;
-    document.getElementById('reset').disabled = true;
-  }
-  else {
-    document.getElementById('rest').disabled = false;
-    document.getElementById('work').disabled = false;
-    document.getElementById('reset').disabled = false;
-  }
-}
 
-function changeTextOfStartButton() {
-  if (!start) {
-    document.getElementById('start').textContent = 'Stop';
-    console.log('changed');
-    start = true;
-  }
-  else {
-    document.getElementById('start').textContent = 'Start';
-    console.log('Start again');
-    start = false;
-  }
-}
 
 
 
@@ -219,252 +199,8 @@ function changeTextOfStartButton() {
 
 
 
-// function runSecondCountDown() {
-//   // let secondTime;
-//   if (document.getElementById('work').checked) {
-//     let secondTime = secondCountDown();
-//     return secondTime;
-//   }
-//   else {
-//     let secondTime = secondCountDown();
-//     return secondTime;
-//   }
-// }
 
-// let workInterval;
-// let restInterval;
 
-// function runTimer() {
-//   let interval = {
-//    workInterval: setInterval(displayWorkTime, 1000),
-//    restInterval: setInterval(displayRestTime,1000)
-//   };
-//   // return document.getElementById('work').checked ? interval.workInterval : interval.restInterval;
-//   let intervalId = document.getElementById('work').checked ? interval.workInterval : interval.restInterval;
-//   console.log(intervalId);
-//   return intervalId;
-// }
-
-
-// function stopTimer(){
-//   document.getElementById('work').checked? clearInterval(setInterval(displayWorkTime, 1000)):clearInterval(setInterval(displayRestTime, 1000));
-// }
-
-// function resetTimer(){
-//   let intervalRef = runTimer();
-//   console.log(intervalRef);
-//   // second = 0;
-//   if(document.getElementById('work').checked){
-//     minute = 25;
-//     second = 0;
-//     workTime.textContent = `${minute}:0${second}`;
-//   }
-  // else{
-  //   restMinute = 5;
-  //   second = 0;
-  //   restTime.textContent = `${restMinute}:0${second}`;
-  // }
-//   console.log(clearTimeout(intervalRef));
-// //  return clearInterval(intervalRef);
-// return clearTimeout(intervalRef);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//the conditon (if (second === 0 && minute > 0)) only works if I declare the second variable global.
-// let second;
-
-//closure function: But how can I use the second variable inside of another function
-// let sec = (function () {
-//    second = 60;
-//   return function () {
-//     if (second > 10 && second <= 60) {
-//       second--;
-//       console.log(second);
-//       return second;
-//     }
-//     else if ((second > 0 && second <= 10)) {
-//       second--;
-//       console.log('0' + second);
-//       return `0${second}`;
-//     }
-//     // else if( second === 0 && minute > 0){
-//     //   second = 60;
-//     // }
-//     else if (second === 0 && minute === 0) {
-//       return;
-//     }
-//     else {
-//       console.log('00');
-//       return `00`;
-//     }
-//   }
-//   })();
-
-
-
-
-
-
-
-// let second = 60;
-
-// function secondTimer() {
-//   // let second = 60;
-//   let sec = document.getElementById('second');
-//   if (second > 10 && second <= 60) {
-//     second--;
-// sec.innerHTML = `${second}`;
-//     console.log(sec.innerHTML);
-//   }
-// }
-
-// document.getElementById('start').addEventListener('click', (e) => {
-//   e.preventDefault();
-//   setInterval(secondTimer, 1000);
-// });
-
-
-
-// let restMinute = 2;
-// let workMinute = 10;
-// let second = 60;
-// let start = false;
-// let workTimer;
-// let restTimer;
-
-// let restTime = document.getElementById("rest-time");
-// let workTime = document.getElementById("work-time");
-
-// // const radioButtons = document.querySelectorAll('form');
-
-// // function startToSTopText() {//working fine
-// //   document.getElementById('start').innerHTML = 'Stop';
-// //   runCountDown();
-// //   document.getElementById('reset').disabled = true;
-// //   console.log('startToStop');
-// //   start = true;
-// // }
-
-// // function stopToStartText() {//this function is not working as expected
-// //   // let myTimer = runCountDown();
-// //   document.getElementById('start').innerHTML = 'Start';
-// //   clearInterval(workTimer);
-// //   document.getElementById('reset').disabled = false;
-// //   console.log('stopToStart');
-// //   start = false;
-// // }
-
-// // //run function based on condition
-// // function run(){
-// //   return start ? stopToStartText() : startToSTopText();
-// // }
-
-// // //do CountDown
-// // function runCountDown() {
-// //   // if(document.getElementById('work').checked){
-// //   workTimer = setInterval(workCountDown, 1000);
-// //   // }
-// //   // else{
-// //    restTimer = setInterval(restCountDown, 1000);
-// // // }
-// //   return (document.getElementById('work').checked) ? workTimer : restTimer;
-// // }
-
-// //run timer here!
-// document.getElementById("start").addEventListener('click', (e) => {
-//   e.preventDefault();
-//   // run();
-//   setInterval(workCountDown,1000);
-//   setInterval(restCountDown,1000);
-// });
-
-// // document.getElementById('reset').addEventListener('click', (e) => {
-// //   e.preventDefault();
-// //   minute = 10;
-// //   second = 0;
-// //   workTime.innerHTML = `${workMinute}:0${second}`;
-// // });
-
-// // //display timer text under desired section
-// // radioButtons.forEach(function (radioButton) {
-// //   // document.getElementById('break').style.display = 'none';
-// //   radioButton.addEventListener('click', () => {
-// //     if (document.getElementById('work').checked) {
-// //       document.getElementById('work-min').style.display = 'block';
-// //       document.getElementById('break').style.display = 'none';
-// //       // console.log('work');
-// //     }
-// //     else {
-// //       document.getElementById('break').style.display = 'block';
-// //       document.getElementById('work-min').style.display = 'none';
-// //       // console.log('rest');
-// //     }
-// //   });
-// // });
-
-// function restCountDown() {
-//   if (second > 10) {
-//     second--;
-//     restTime.innerHTML = `0${restMinute}:${second}`;
-//     console.log(restTime.innerHTML);
-//   } else if (second > 0 && second <= 10) {
-//     second--;
-//     restTime.innerHTML = `0${restMinute}:0${second}`;
-//     console.log(restTime.innerHTML);
-//   } else if (second === 0 && restMinute > 0) {
-//     restMinute--;
-//     second = 59;
-//     restTime.innerHTML = `0${restMinute}:${second}`;
-//     console.log(restTime.innerHTML);
-//   }
-// }
-
-// function workCountDown() {
-//   if (second > 10) {
-//     second--;
-//     workTime.innerHTML = `${workMinute}:${second}`;
-//     // console.log(workTime.innerHTML);
-//     if (workMinute < 10) {
-//       workTime.innerHTML = `0${workMinute}:${second}`;
-//       console.log(workTime.innerHTML);
-//     }
-//   } else if (second > 0 && second <= 10) {
-//     second--;
-//     workTime.innerHTML = `${workMinute}:0${second}`;
-//     // console.log(workTime.innerHTML);
-//     if (workMinute < 10) {
-//       workTime.innerHTML = `0${workMinute}:0${second}`;
-//       console.log(workTime.innerHTML);
-//     }
-//   } else if (second === 0 && workMinute > 0) {
-//     workMinute--;
-//     second = 59;
-//     workTime.innerHTML = `${workMinute}:${second}`;
-//     // console.log(workTime.innerHTML);
-//     if (workMinute < 10) {
-//       workTime.innerHTML = `0${workMinute}:${second}`;
-//       console.log(workTime.innerHTML);
-//     }
-//   }
-// }
 
 
 
